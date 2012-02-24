aec63c49ce245df474d36d58c39afb03bf3c4616
Dylan Egan <dylanegan@gmail.com>
2012-02-24 11:45:47 -0800

    Support account aliases. Add a group source. Pass objects about. :v:

--- lib/fog/bouncer.rb
+++ lib/fog/bouncer.rb
@@ -21,21 +21,30 @@ module Fog
         instance_eval(&block)
       end
 
+      def accounts
+        @accounts ||= {}
+      end
+
+      def account(name, account_id)
+        accounts[name] = account_id
+      end
+
       def groups
         @groups ||= []
       end
 
       def group(name, description, &block)
-        groups << Group.new(name, description, &block)
+        groups << Group.new(name, description, self, &block)
       end
     end
 
     class Group
-      attr_reader :name, :description
+      attr_reader :name, :description, :security
 
-      def initialize(name, description, &block)
+      def initialize(name, description, security, &block)
         @name = name
         @description = description
+        @security = security
         instance_eval(&block) if block_given?
       end
 
@@ -44,23 +53,16 @@ module Fog
       end
 
       def source(source, &block)
-        sources << Sources.for(source, &block)
-      end
-    end
-
-    class Sources
-      def self.for(source, &block)
-        if source =~ /^\d+\.\d+\.\d+.\d+\/\d+$/
-          CIDRSource.new(source, &block)
-        else
-          GroupSource.new(source, &block)
-        end
+        sources << Sources.for(source, self, &block)
       end
     end
 
     class Source
-      def initialize(source, &block)
+      attr_reader :group
+
+      def initialize(source, group, &block)
         @source = source
+        @group = group
         instance_eval(&block) if block_given?
       end
 
@@ -69,44 +71,79 @@ module Fog
       end
 
       def icmp(*ports)
-        ports.each { |port| protocols[:icmp] << ICMP.new(port) }
+        ports.each { |port| protocols[:icmp] << Fog::Bouncer::Protocols::ICMP.new(port, self) }
       end
 
       def tcp(*ports)
-        ports.each { |port| protocols[:tcp] << TCP.new(port) }
+        ports.each { |port| protocols[:tcp] << Fog::Bouncer::Protocols::TCP.new(port, self) }
       end
 
       def udp(*ports)
-        ports.each { |port| protocols[:udp] << UDP.new(port) }
+        ports.each { |port| protocols[:udp] << Fog::Bouncer::Protocols::UDP.new(port, self) }
       end
     end
 
-    class CIDRSource < Source
-      def range
-        @source
+    module Sources
+      def self.for(source, group, &block)
+        if source =~ /^\d+\.\d+\.\d+.\d+\/\d+$/
+          CIDR.new(source, group, &block)
+        else
+          Group.new(source, group, &block)
+        end
+      end
+
+      class CIDR < Fog::Bouncer::Source
+        def range
+          @source
+        end
+      end
+
+      class Group < Fog::Bouncer::Source
+        attr_reader :user_id, :user_alias, :name
+
+        def initialize(source, group, &block)
+          super
+          case source
+          when /^(.+)@(.+)$/
+            @name = $1
+            @user_alias = $2
+          when /^@(.+)$/
+            @user_alias = $1
+          else
+            @name = source
+          end
+        end
+
+        def user_id
+          group.security.accounts[user_alias]
+        end
       end
     end
 
     class Protocol
       attr_reader :from, :to
 
-      def initialize(port)
+      def initialize(port, source)
         if port.is_a?(Range)
           @from = port.begin
           @to = port.end
         else
           @from = port
         end
+
+        @source = source
       end
     end
 
-    class ICMP < Protocol
-    end
+    module Protocols
+      class ICMP < Protocol
+      end
 
-    class TCP < Protocol
-    end
+      class TCP < Protocol
+      end
 
-    class UDP < Protocol
+      class UDP < Protocol
+      end
     end
   end
 end
--- spec/fog/bouncer_spec.rb
+++ spec/fog/bouncer_spec.rb
@@ -12,8 +12,16 @@ describe Fog::Bouncer do
   describe "#security" do
     before do
       @doorlist = Fog::Bouncer.security do
+        account "jersey_shore", "1234567890"
+
         group "douchebag", "Don't let them in!" do
           source "1.1.1.1/1" do
+            tcp 7070..8080, 80
+          end
+        end
+
+        group "guido", "Definitely don't let them in!" do
+          source "douchebag@jersey_shore" do
             tcp 7070..8080
           end
         end
@@ -26,13 +34,23 @@ describe Fog::Bouncer do
       douchebag.description.must_equal "Don't let them in!"
 
       source = douchebag.sources.first
-      source.must_be_kind_of Fog::Bouncer::CIDRSource
+      source.must_be_kind_of Fog::Bouncer::Sources::CIDR
       source.range.must_equal "1.1.1.1/1"
 
       protocol = source.protocols[:tcp].first
-      protocol.must_be_kind_of Fog::Bouncer::TCP
+      protocol.must_be_kind_of Fog::Bouncer::Protocols::TCP
       protocol.from.must_equal 7070
       protocol.to.must_equal 8080
+    end
+
+    it "has a guido group" do
+      guido = @doorlist.groups.find { |g| g.name == "guido" }
+
+      source = guido.sources.first
+      source.must_be_kind_of Fog::Bouncer::Sources::Group
+      source.user_alias.must_equal "jersey_shore"
+      source.user_id.must_equal "1234567890"
+      source.name.must_equal "douchebag"
     end
   end
 end
