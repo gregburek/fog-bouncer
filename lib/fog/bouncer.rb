2e186b6db448a4e930edbf12a215a61700e831f2
Greg Burek <gburek@salesforce.com>
2012-07-09 16:38:55 -0700

    typo fix

--- lib/fog/bouncer.rb
+++ lib/fog/bouncer.rb
@@ -57,9 +57,7 @@ module Fog
         :region => (ENV['PROVIDER_REGION'] || 'us-east-1'),
         :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
         :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :connection_options => {:ssl_verify_peer => ENV["DISABLE_PROVIDER_SSL_VERIFY_PEER"] != "1"}
-    }
-  end
+        :connection_options => {:ssl_verify_peer => ENV["DISABLE_PROVIDER_SSL_VERIFY_PEER"] != "1"}
       )
     end
 
